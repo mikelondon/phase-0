62ec2a2d60839d4d7bffb8b861b265761e262950
Mike London <mikelawndawn@gmail.com>
2016-02-13 12:08:52 -0800

    Final Solution

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -16,26 +16,38 @@ undefined
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 
-git/*
+
 for (var num = 1; num <= 100; num++) {
-  if (num % 3 === 0) {
+  if(num % 3 === 0 && num % 5 === 0)  {
     console.log('Fizz');
   } else if (num % 5 === 0) {
     console.log('Buzz');
-  } else if (num % 3 === 0 && num % 5 === 0){
+  } else if (num % 3 === 0) {
     console.log('FizzBuzz');
   } else {
     console.log(num);
   }
 }
-*/
+
 
 // Functions
 
-// Complete the `minimum` exercise.
 
+// Complete the `minimum` exercise.
+function min (a,b){
+  if (a < b)
+    return a;
+  else
+    return b;
+}
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
-// Status API Training Shop Blog About Pricing
+
+var me = {
+  name: "mike",
+  age: 23,
+  favoriteFood: "Pizza",
+  quirk: "I've been in a coma",
+};
