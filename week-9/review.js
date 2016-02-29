c53caf1d84c65ffb76ae8b908756d293a3c083ff
Mike London <mikelawndawn@gmail.com>
2016-02-28 20:24:03 -0800

    Finish 9.2

--- week-9/review.js
+++ week-9/review.js
@@ -30,14 +30,19 @@ print the smallest number
 */
 
 // Solution
+
 function smallest_num(list_of_nums) {
   var small_num = list_of_nums[0];
   for (var i = 0; i < list_of_nums.length; i++) {
-    if variable < small_num;
-      small_num = variable;
+    if(list_of_nums[i] < small_num){
+      small_num = list_of_nums[i];
+    }
   };
   console.log(small_num)
   }
+
+smallest_num([2,5,9,1])
+// ==> 1
 
 
 //  Reflection
@@ -45,16 +50,16 @@ function smallest_num(list_of_nums) {
 /*
 
 What concepts did you solidify in working on this challenge?
-  -
+  - Iteration and picking things out of an array.
 
 What was the most difficult part of this challenge?
-  -
+  - I chose to work on this problem becasue it was the inital solo challenge I found the most trouble with. It was the first week of Ruby and I spent almost 8 hours tryign to get past the first part. I am happy to say I was able to translate it to ruby fairly simple.
 
 Did you solve the problem in a new way this time?
-  -
+  - I initally tried to but I wanted to see if if could do a 'translation' of it from ruby.
 
 Was your pseudocode different from the Ruby version? What was the same and what was different?
-  -
+  - Yes I thought it was different as I was able to logically think out the problem better than before.
 
 
 */
