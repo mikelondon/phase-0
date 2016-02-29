dd0ce48b8f88d9125af99f97764d701e7666e7af
Mike London <mikelawndawn@gmail.com>
2016-02-28 20:03:00 -0800

    Finish assignments

---
+++ week-9/review.js
@@ -0,0 +1,44 @@
+
+// Original Ruby Solution
+
+// def smallest_integer(list_of_nums)
+//   return nil if list_of_nums == []
+
+//   small_num = list_of_nums[0]
+//     list_of_nums.each do |num|
+//       if num < small_num
+//         small_num = num
+//       end
+//     end
+//     p small_num
+//   end
+
+// Psuedocode
+
+/*
+
+
+*/
+
+// Solution
+
+
+
+//  Reflection
+
+/*
+
+What concepts did you solidify in working on this challenge?
+  -
+
+What was the most difficult part of this challenge?
+  -
+
+Did you solve the problem in a new way this time?
+  -
+
+Was your pseudocode different from the Ruby version? What was the same and what was different?
+  -
+
+
+*/
